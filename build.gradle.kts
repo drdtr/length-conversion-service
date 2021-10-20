bd8ac7bf014ea3dc22268e348c67e6e199454ead
David Trachtenherz <david.trachtenherz@rio.cloud>
2021-10-20 23:21:51 +0200

    setup project

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.5.5"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.5.31"
+	kotlin("plugin.spring") version "1.5.31"
+}
+
+group = "me.david"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	val springdocVersion = "1.5.11"
+	implementation("org.springdoc:springdoc-openapi-ui:$springdocVersion")
+	implementation("org.springdoc:springdoc-openapi-kotlin:$springdocVersion")
+	implementation("org.springdoc:springdoc-openapi-common:$springdocVersion")
+
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation(kotlin("test-junit"))
+	testImplementation("io.mockk:mockk:1.12.0")
+	testImplementation("com.ninja-squad:springmockk:3.0.1")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/me/david/lengthconversionservice/LengthConversionServiceApplication.kt
@@ -0,0 +1,11 @@
+package me.david.lengthconversionservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LengthConversionServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LengthConversionServiceApplication>(*args)
+}
